3f0c24d2a482f2049125db10472f8894123e9438
BIPLOV KUMAR <biplov@tech9logy.in>
2020-10-08 18:25:49 +0530

    running with rnfirebase12

--- android/app/src/main/java/com/firebasedemo/MainApplication.java
+++ android/app/src/main/java/com/firebasedemo/MainApplication.java
@@ -12,6 +12,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,8 +30,12 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-            packages.add(new RNFirebaseMessagingPackage());
+packages.add(new RNFirebaseMessagingPackage());
          packages.add(new RNFirebaseNotificationsPackage());
+            packages.add(new RNFirebaseFirestorePackage());
+
+                   packages.add(new RNFirebaseAuthPackage());
+
 
           return packages;
         }
