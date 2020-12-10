07e41a1907cdc2341fa907576d6c446245ef7ead
t9l_vikas <vikas@tech9logy.in>
2020-12-10 17:38:06 +0530

    load more

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,108 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import styles from './styles';
-import { firebase } from '../../firebase/config'
+import React, { Component } from 'react'
+import { Text, View, Button, StyleSheet, FlatList, Dimensions, ActivityIndicator, TextInput, Keyboard, ImageBackground, TouchableOpacity, Image } from 'react-native'
+const { width, height } = Dimensions.get("window");
 
-export default function HomeScreen(props) {
+export const MinMargin = width / 40;
+export const Margin = width / 20;
+export default class HomeScreen extends Component {
+    constructor(props) {
+        super(props)
 
-    const [entityText, setEntityText] = useState('')
-    const [entities, setEntities] = useState([])
-
-    const entityRef = firebase.firestore().collection('entities')
-    const userID = props.extraData.id
-    console.log("props",props);
-
-    useEffect(() => {
-        entityRef
-            .where("authorID", "==", userID)
-            .orderBy('createdAt', 'desc')
-            .onSnapshot(
-                querySnapshot => {
-                    const newEntities = []
-                    querySnapshot.forEach(doc => {
-                        const entity = doc.data()
-                        entity.id = doc.id
-                        newEntities.push(entity)
-                    });
-                    setEntities(newEntities)
-                    console.log('====================================');
-                    console.log(newEntities,"props",props);
-                    console.log('====================================');
-                },
-                error => {
-                    console.log(error)
-                }
-            )
-    }, [])
-
-    const onAddButtonPress = () => {
-        if (entityText && entityText.length > 0) {
-            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-            const data = {
-                text: entityText,
-                authorID: userID,
-                createdAt: timestamp,
-            };
-            entityRef
-                .add(data)
-                .then(_doc => {
-                    setEntityText('')
-                    Keyboard.dismiss()
-                })
-                .catch((error) => {
-                    alert(error)
-                });
+        this.state = {
+            imgData: [],
+            loading: false,
+            load: 6,
+            AllData: []
         }
     }
+    componentDidMount() {
+        this.getImageData()
+    }
+    getImageData() {
+        this.setState({ loading: true })
+        let url = 'https://jsonplaceholder.typicode.com/photos'
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log('-------', res);
+                this.setState({ loading: false })
 
-    const renderEntity = ({item, index}) => {
+                if (res && res != "") {
+                    let arr = []
+                    res.map((item, index) => {
+                        if (index < this.state.load) {
+                            arr.push(item)
+                        }
+                    })
+                    this.setState({ AllData: res, imgData: arr })
+                }
+            })
+            .catch((error) => {
+                console.log("ERROR ", error)
+                this.setState({ loading: false })
+
+            })
+
+    }
+
+
+    renderFooter = () => {
         return (
-            <View style={styles.entityContainer}>
-                <Text style={styles.entityText}>
-                    {index}. {item.text}
-                </Text>
+            <View
+                style={{ height: height / 15, width: width, borderTopWidth: 1 }}>
+                <TouchableOpacity onPress={() => this.handleLoadMore()}
+                    style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: height / 45 }}>Load More</Text></TouchableOpacity>
             </View>
         )
     }
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.formContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Add new entity'
-                    placeholderTextColor="#aaaaaa"
-                    onChangeText={(text) => setEntityText(text)}
-                    value={entityText}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
+    renderItem = ({ item, index }) => {
+        const { imgData, load } = this.state
+
+        return (
+            <View
+                style={{ backgroundColor: "#fff", paddingVertical: Margin, width: width / 2, paddingLeft: index % 2 == 0 ? MinMargin / 2 : MinMargin }}>
+                <Image
+                    source={{ uri: item.url, cache: 'force-cache' }} style={{ width: (width - (MinMargin * 3)) / 2, height: (width - (MinMargin * 3)) / 1.5, }} resizeMode="cover"
                 />
-                <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-                    <Text style={styles.buttonText}>Add</Text>
-                </TouchableOpacity>
+
             </View>
-            { entities && (
-                <View style={styles.listContainer}>
-                    <FlatList
-                        data={entities}
-                        renderItem={renderEntity}
-                        keyExtractor={(item) => item.id}
-                        removeClippedSubviews={true}
-                    />
+        )
+    }
+    handleLoadMore = () => {
+        const { AllData, load } = this.state
+        this.setState({ loading: true })
+        let arr = []
+        let add = load + 6
+        this.setState({ load: add })
+        AllData.map((item, index) => {
+            if (index < add) {
+                arr.push(item)
+            }
+        })
+        this.setState({ imgData: arr })
+        setTimeout(() => {
+            this.setState({ loading: false })
+        }, 400);
+    }
+    render() {
+        const { AllData, imgData, load, loading } = this.state
+        if (loading) {
+            return (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size={'large'} color={"orange"} />
                 </View>
-            )}
-        </View>
-    )
+            )
+        }
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 9, }}>
+                    {imgData.length > 0 ?
+                        <FlatList
+                            keyboardShouldPersistTaps='handled'
+                            data={imgData}
+                            renderItem={this.renderItem}
+                            numColumns={2}
+                            showsVerticalScrollIndicator={false}
+                            ListFooterComponent={() => this.renderFooter()}
+                            keyExtractor={(item, index) => index.toString()}
+                        /> : <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>No Data Found</Text></View>
+                    }
+
+                </View>
+            </View>
+        )
+    }
 }
-
-
-
-
-// service cloud.firestore {
-//     match /databases/{database}/documents {
-//       match /{document=**} {
-//         allow read, write: if
-//             request.time < timestamp.date(2020, 11, 8);
-//       }
-//     }
-//   }
